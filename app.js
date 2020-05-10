75e3005450dcac898845c238f0fba6e160c812ca
Wai Lam Fergus Yip <z5248282@unsw.edu.au>
2020-05-10 15:48:12 +1000

    Add mongoose

--- app.js
+++ app.js
@@ -2,9 +2,12 @@ const express = require('express');
 const app = express()
 const morgan = require('morgan'); // Logging package
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 
 const cardRoutes = require('./api/routes/card');
+
+mongoose.connect(process.env.MONGO_ATLAS_URI, { useMongoClient: true });
 
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended: false }));
