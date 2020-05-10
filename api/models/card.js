a1aa1cc07d56f12a695dac860ae42bd126b579d3
Wai Lam Fergus Yip <fergus.yipwailam@gmail.com>
2020-05-10 17:36:59 +1000

    Required fields

--- api/models/card.js
+++ api/models/card.js
@@ -2,8 +2,8 @@ const mongoose = require("mongoose");
 
 const cardSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
-    prompt: String,
-    answer: String,
+    prompt: { type: String, required: true },
+    answer: { type: String, required: true },
 });
 
 module.exports = mongoose.model("Card", cardSchema);
