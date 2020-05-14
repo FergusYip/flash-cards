2ab49dac285c98aea7a53a5047c1dcbd8d45089e
Wai Lam Fergus Yip <fergus.yipwailam@gmail.com>
2020-05-14 14:36:21 +1000

    regex for email

--- api/models/user.js
+++ api/models/user.js
@@ -1,12 +1,17 @@
 const mongoose = require("mongoose");
 
 const userSchema = mongoose.Schema({
-    _id: mongoose.Schema.Types.ObjectId,
-    email: { type: String, required: true },
-    password: { type: String, required: true },
-    name: { type: String, required: true },
-    cards: [{ type: mongoose.Schema.Types.ObjectId, ref: "Card" }],
-    stacks: [{ type: mongoose.Schema.Types.ObjectId, ref: "Stack" }],
+  _id: mongoose.Schema.Types.ObjectId,
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/,
+  },
+  password: { type: String, required: true },
+  name: { type: String, required: true },
+  cards: [{ type: mongoose.Schema.Types.ObjectId, ref: "Card" }],
+  stacks: [{ type: mongoose.Schema.Types.ObjectId, ref: "Stack" }],
 });
 
 module.exports = mongoose.model("User", userSchema);
