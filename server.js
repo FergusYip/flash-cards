2e901c2a8007f5dd7885598690714ac61a9d0a7d
Wai Lam Fergus Yip <fergus.yipwailam@gmail.com>
2020-05-17 18:09:33 +1000

    use dotenv

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
-const http = require('http');
-const app = require('./app');
+require("dotenv").config();
 
-const port = process.env.port || 3000;
+const http = require("http");
+const app = require("./app");
+
+const port = process.env.PORT || 3000;
 const server = http.createServer(app);
-server.listen(port)
+server.listen(port);
+console.log("SERVER: Listening on port " + port);
