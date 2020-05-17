8e336f3bd0f5db7c1e4d1a2e8c81fcea8668dd9e
Wai Lam Fergus Yip <fergus.yipwailam@gmail.com>
2020-05-18 01:16:36 +1000

    clean server file

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 require("dotenv").config();
 
-const http = require("http");
 const app = require("./app");
-
 const port = process.env.PORT || 3000;
-const server = http.createServer(app);
-server.listen(port);
-console.log("SERVER: Listening on port " + port);
+
+app.listen(port, () => {
+  console.log("Server listening on port " + port);
+});
