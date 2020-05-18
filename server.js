bd267b146d3e749ca900aec9bd866841f3ce886c
Fergus Yip <fergus.yipwailam@gmail.com>
2020-05-18 14:40:47 +1000

    Moved mongoose connection to server.js

--- server.js
+++ server.js
@@ -1,5 +1,17 @@
 require("dotenv").config();
 
+const mongoose = require("mongoose");
+
+mongoose.connect(process.env.MONGO_ATLAS_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
+
+let db = mongoose.connection;
+db.on("error", console.error.bind(console, "Mongooose: Connection error"));
+
 const app = require("./app");
 const port = process.env.PORT || 3000;
 
