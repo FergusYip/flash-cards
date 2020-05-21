508d094f347eed545fa8adc0c1aaa0d3a0fd6779
Wai Lam Fergus Yip <fergus.yipwailam@gmail.com>
2020-05-21 15:14:59 +1000

    card db layer

--- api/db/cards.js
+++ api/db/cards.js
@@ -4,8 +4,51 @@ const Card = require("../models/cards");
 const Stack = require("../models/stacks");
 const User = require("../models/user");
 
+exports.getAllCardsDB = async () => {
+  const cards = await Card.find().select("_id prompt answer").exec();
+  return cards.map((card) => card.transform());
+};
+
+exports.createCardDB = async (prompt, answer) => {
+  const card = new Card({
+    _id: new mongoose.Types.ObjectId(),
+    prompt: prompt,
+    answer: answer,
+  });
+  await card.save();
+  return card.transform();
+};
+
 exports.getCardDB = async (cardId) => {
   const card = await Card.findById(cardId).select("_id prompt answer").exec();
+
+  if (!card) {
+    throw new Error("No valid entry found for provided cardId.");
+  }
+
+  return card.transform();
+};
+
+exports.setCardPromptDB = async (cardId, prompt) => {
+  const card = await Card.findByIdAndUpdate(
+    cardId,
+    {
+      $set: { prompt: prompt },
+    },
+    { new: true }
+  )
+    .select("_id prompt answer")
+    .exec();
+
+  if (!card) {
+    throw new Error("No valid entry found for provided cardId.");
+  }
+
+  return card.transform();
+};
+
+exports.deleteCardDB = async (cardId) => {
+  const card = await Card.findByIdAndDelete(cardId).exec();
 
   if (!card) {
     throw new Error("No valid entry found for provided cardId.");
