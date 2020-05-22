724e48b94ce15e23a0ead01f02d7c4d2ae9ce8f0
Fergus Yip <fergus.yipwailam@gmail.com>
2020-05-22 20:38:08 +1000

    remove old controllers

--- backend/api/controllers/cards.js
+++ backend/api/controllers/cards.js
@@ -47,59 +47,6 @@ exports.createCardController = async (req, res, next) => {
   }
 };
 
-exports.cards_create_card = (req, res, next) => {
-  const userId = req.tokenPayload.userId;
-  const { prompt, answer } = req.body;
-  let stackId = req.body.stackId;
-
-  User.findById(userId)
-    .exec()
-    .then((user) => {
-      const card = new Card({
-        _id: new mongoose.Types.ObjectId(),
-        prompt: prompt,
-        answer: answer,
-      });
-
-      if (stackId == null) {
-        console.log("stackId is null");
-        stackId = user.defaultStack;
-      }
-      console.log(stackId);
-      return Promise.all([
-        card.save(),
-        Stack.findByIdAndUpdate(
-          stackId,
-          { $addToSet: { cards: card } },
-          { new: true }
-        )
-          .select("cards _id name")
-          .exec(),
-      ]);
-    })
-    .then((result) => {
-      const [card, stack] = result;
-      if (stack == null) {
-        console.log("after promise: stack is null");
-      }
-      return res.status(200).json({
-        message: "Created new card successfully",
-        card: {
-          cardId: card._id,
-          prompt: card.prompt,
-          answer: card.answer,
-        },
-        stack: stack.transform(),
-      });
-    })
-    .catch((err) => {
-      console.log(err);
-      return res.status(500).json({
-        error: err,
-      });
-    });
-};
-
 exports.getCardController = async (req, res, next) => {
   const cardId = req.params.cardId;
   try {
@@ -111,29 +58,6 @@ exports.getCardController = async (req, res, next) => {
       error: err.message,
     });
   }
-};
-
-exports.cards_get_card = (req, res, next) => {
-  const id = req.params.cardId;
-
-  Card.findById(id)
-    .exec()
-    .then((doc) => {
-      console.log(doc);
-      if (doc) {
-        res.status(200).json(doc);
-      } else {
-        res.status(404).json({
-          message: "No valid entry found for provided ID.",
-        });
-      }
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json({
-        error: err,
-      });
-    });
 };
 
 exports.setCardPromptController = async (req, res, next) => {
@@ -184,47 +108,4 @@ exports.setCardAnswerController = async (req, res, next) => {
       error: err,
     });
   }
-};
-
-exports.cards_patch_card = (req, res, next) => {
-  const id = req.params.cardId;
-  const updateOps = {};
-  for (const ops of req.body) {
-    console.log(ops);
-    updateOps[ops.propName] = ops.value;
-  }
-  console.log(updateOps);
-  Card.updateOne(
-    { _id: id },
-    {
-      $set: updateOps,
-    }
-  )
-    .exec()
-    .then((result) => {
-      console.log(result);
-      res.status(200).json(result);
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json({
-        error: err,
-      });
-    });
-};
-
-exports.cards_delete_card = (req, res, next) => {
-  const id = req.params.cardId;
-
-  Card.remove({ _id: id })
-    .exec()
-    .then((result) => {
-      res.status(200).json(result);
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json({
-        error: err,
-      });
-    });
 };
