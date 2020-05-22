84ededdb5c9b74e21af6ab1e54252f21b4dc2ccb
Fergus Yip <fergus.yipwailam@gmail.com>
2020-05-22 16:56:34 +1000

    move check vars to server.js

--- backend/app.js
+++ backend/app.js
@@ -9,9 +9,6 @@ const userRoutes = require("./api/routes/user");
 const cardRoutes = require("./api/routes/cards");
 const stackRoutes = require("./api/routes/stacks");
 
-const checkEnvVars = require("./check-env");
-checkEnvVars();
-
 app.use(morgan("dev"));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
