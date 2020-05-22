938a5ca4422ed23886d0e2107b395c35b3f19e93
Fergus Yip <fergus.yipwailam@gmail.com>
2020-05-22 16:52:31 +1000

    change import path

--- backend/api/routes/stacks.js
+++ backend/api/routes/stacks.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const checkAuth = require("../auth/check-auth");
+const checkAuth = require("../middleware/check-auth");
 
 const StacksController = require("../controllers/stacks");
 
